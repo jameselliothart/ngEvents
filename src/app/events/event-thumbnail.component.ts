7883352c3a92514d7570e8e5df87e38e64b36bf6
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-09-12 06:15:48 -0500

    display list of events

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -14,6 +14,7 @@ import { Input, Component } from "@angular/core";
     </div>
   `,
   styles: [`
+    .thumbnail {min-height: 210px;}
     .pad-left {margin-left: 10px;}
     .well div {color: #bbb;}
   `]
