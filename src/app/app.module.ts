2b0fb7d1a6d1e5fd314505084d0802b4fd6eea8d
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-09-14 15:20:15 -0500

    check navigation away from create event page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,8 +31,19 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
   providers: [
     EventService,
     ToastrService,
-    EventRouteActivator
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event. Do you really want to cancel?');
+  }
+  return true;
+}
